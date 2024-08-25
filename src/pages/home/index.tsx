9097d3dc7e0c68f1ff8971aa1e3bd55c18c05636
Rain <iheyu22@gmail.com>
2024-08-25 17:18:49 +0800

    update eslint config

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,27 @@
-import {View, Text, ScrollView} from '@tarojs/components'
+import {ScrollView, Text, View} from '@tarojs/components'
+import {TabBar} from 'antd-mobile';
+import {AppOutline, UserOutline} from 'antd-mobile-icons'
 import './index.scss'
-import {Avatar, TabBar} from "antd-mobile";
-import {AppOutline, MessageOutline, UserOutline} from 'antd-mobile-icons'
 
 
 export default function Index() {
 
   return (
     <View className='homeContainer'>
-      <ScrollView className={'scroll'} scrollY>
-        <View className={'appName'}>
-          <Text>{'MTGG'}</Text>
+      <ScrollView className='scroll' scrollY>
+        <View className='appName'>
+          <Text>MTGG4</Text>
         </View>
-        <View className={'userInfo'}>
-          <Avatar className={'userAvatar'} size={'large'} src={'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'}/>
-          <Text>{'用户昵称'}</Text>
+        <View className='userInfo'>
+          <Text>用户昵称3</Text>
         </View>
-        <View className={'moduleBox'}></View>
-        <View className={'bottomBox'}></View>
+        <View className='moduleBox'></View>
+        <View className='bottomBox'></View>
       </ScrollView>
-      <TabBar className={'tabBar'} activeKey={'home'}>
-        <TabBar.Item key="home" icon={<AppOutline/>}/>
+      <TabBar className='tabBar' activeKey='home'>
+        <TabBar.Item key='home' icon={<AppOutline />} />
         {/*<TabBar.Item key="message" icon={<MessageOutline/>}/>*/}
-        <TabBar.Item key="user" icon={<UserOutline/>}/>
+        <TabBar.Item key='user' icon={<UserOutline />} />
       </TabBar>
     </View>
   )
